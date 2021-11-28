50fcad70a67c5b8c537bcfd8060816e0061dc6cf
AndreiMusca00 <andrei.m150@Yahoo.com>
2021-11-28 14:29:23 +0200

    Add project files.

---
+++ Data/Musca_Andrei_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Musca_Andrei_Lab8.Models;
+
+namespace Musca_Andrei_Lab8.Data
+{
+    public class Musca_Andrei_Lab8Context : DbContext
+    {
+        public Musca_Andrei_Lab8Context (DbContextOptions<Musca_Andrei_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Musca_Andrei_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Musca_Andrei_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Musca_Andrei_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Musca_Andrei_Lab8.Data;
+using Musca_Andrei_Lab8.Models;
+
+namespace Musca_Andrei_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Musca_Andrei_Lab8.Data.Musca_Andrei_Lab8Context _context;
+
+        public IndexModel(Musca_Andrei_Lab8.Data.Musca_Andrei_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.Include(b => b.Publisher).ToListAsync();
+        }
+    }
+}
